5208090e7f812310283448b47b12b75fdf5f2c3c
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-09-16 10:05:57 +0800

    Adding JSON schemas and pydantic Base models

--- src/pymaml/__init__.py
+++ src/pymaml/__init__.py
@@ -1,15 +1,9 @@
 # src/pymaml/__init__.py
 
-# Import the main classes from maml.py
 from .maml import Field, MAML
-
-# Import the constants from parse.py (these are what your tests are looking for)
 from .parse import FIELD_KEY_ORDER, MAML_KEY_ORDER, is_valid, is_iso8601
-
-# Import functions from read.py
 from .read import read_maml
 
-# Export everything your tests and users need
 __all__ = [
     "Field",
     "MAML", 
